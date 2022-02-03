1e3da9c2b49d40c7663ea72bcf6d8ad7f6ec73bd
sahatto <91620068+sahatto@users.noreply.github.com>
2022-02-03 10:23:45 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -53,6 +53,9 @@ function continentChoice () {
     game.showLongText("1:North America   2:South America   3:Africa", DialogLayout.Center)
     game.showLongText("4:Australia   5:Asia   6:Europe", DialogLayout.Center)
     game.splash(game.askForNumber("", 1))
+    if (0 == 1) {
+    	
+    }
 }
 scene.setBackgroundColor(7)
 game.setDialogFrame(img`
